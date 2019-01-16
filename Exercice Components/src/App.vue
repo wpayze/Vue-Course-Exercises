db61bcd959a8881ad9a412556de581003ebbf556
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2019-01-16 15:46:35 -0600

    COMPONENTES

---
+++ Exercice Components/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <wp-header></wp-header>
+        <hr>
+        <wp-servers></wp-servers>
+        <hr>
+        <wp-footer></wp-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Servers from './Servers.vue';
+    import Footer from './Footer.vue';
+
+    export default {
+        components: {
+            'wp-header': Header,
+            'wp-servers': Servers,
+            'wp-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Exercice Components/src/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+    </div>
+</template>
