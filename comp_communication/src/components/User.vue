9f0b1ca2b1802a08e716836028fc7bfcfc6fa971
Wilfredo Paiz <wilfredopaizpaz@gmail.com>
2019-01-24 12:03:16 -0600

    Components Communication

---
+++ comp_communication/src/components/User.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <input type="text" v-model="nombre">
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :nombre="nombre"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        data: function(){
+            return {
+                nombre: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
